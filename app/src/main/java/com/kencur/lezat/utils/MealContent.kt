dd3827a4e311e07e1c51ce734cbad2e9469cf7ba
Julio Rahman <julio.rahman@gmail.com>
2021-05-30 19:34:34 +0700

    fix (meal content): add more dummy data

--- app/src/main/java/com/kencur/lezat/utils/MealContent.kt
+++ app/src/main/java/com/kencur/lezat/utils/MealContent.kt
@@ -10,6 +10,34 @@ object MealContent {
         MEAL.apply {
             add(
                 Meal(
+                    "Pumpkin Pie",
+                    "Dessert",
+                    "American",
+                    listOf(
+                        "Place the pumpkin in a large saucepan, cover with water and bring to the boil.",
+                        "Cover with a lid and simmer for 15 mins or until tender.",
+                        "Drain pumpkin, let cool.",
+                        "Heat oven to 180\u00B0C/160\u00B0C fan/gas 4.",
+                        "Roll out the pastry on a lightly floured surface and use it to line a 22cm loose-bottomed tart tin.",
+                        "Chill for 15 mins.",
+                        "Line the pastry with baking parchment and baking beans, then bake for 15 mins.",
+                        "Remove the beans and paper, and cook for a further 10 mins until the base is pale golden and biscuity.",
+                        "Remove from the oven and allow to cool slightly.",
+                        "Increase oven to 220\u00B0C/200\u00B0C fan/gas 7.",
+                        "Push the cooled pumpkin through a sieve into a large bowl.",
+                        "In a separate bowl, combine the sugar, salt, nutmeg and half the cinnamon.",
+                        "Mix in the beaten eggs, melted butter and milk, then add to the pumpkin purée and stir to combine.",
+                        "Pour into the tart shell and cook for 10 mins, then reduce the temperature to 180\u00B0C/160\u00B0C fan/gas 4.",
+                        "Continue to bake for 35-40 mins until the filling has just set.",
+                        "Leave to cool, then remove the pie from the tin.",
+                        "Mix the remaining cinnamon with the icing sugar and dust over the pie.",
+                        "Serve chilled."
+                    ),
+                    "https://www.themealdb.com/images/media/meals/usuqtp1511385394.jpg"
+                )
+            )
+            add(
+                Meal(
                     "Big Mac",
                     "Beef",
                     "American",
@@ -68,6 +96,143 @@ object MealContent {
                         "Serve with lemon wedges."
                     ),
                     "https://www.themealdb.com/images/media/meals/tyywsw1505930373.jpg",
+                )
+            )
+            add(
+                Meal(
+                    "Japanese Katsudon",
+                    "Pork",
+                    "Japanese",
+                    listOf(
+                        "Heat the oil in a pan, fry the sliced onion until golden brown, then add the tonkatsu, placing it in the middle of the pan.",
+                        "Mix the dashi, soy, mirin and sugar together and tip three-quarters of the mixture around the tonkatsu.",
+                        "Sizzle for a couple of mins so the sauce thickens a little and the tonkatsu reheats.",
+                        "Tip the beaten eggs around the tonkatsu and cook for 2-3 mins until the egg is cooked through but still a little runny.",
+                        "Divide the rice between two bowls, then top each with half the egg and tonkatsu mix, sprinkle over the chives and serve immediately, drizzling with a little more soy if you want an extra umami kick."
+                    ),
+                    "https://www.themealdb.com/images/media/meals/d8f6qx1604182128.jpg"
+                )
+            )
+            add(
+                Meal(
+                    "Yaki Udon",
+                    "Vegetarian",
+                    "Japanese",
+                    listOf(
+                        "Boil some water in a large saucepan.",
+                        "Add 250ml cold water and the udon noodles. (As they are so thick, adding cold water helps them to cook a little bit slower so the middle cooks through).",
+                        "If using frozen or fresh noodles, cook for 2 mins or until al dente; dried will take longer, about 5-6 mins.",
+                        "Drain and leave in the colander.",
+                        "Heat 1 tbsp of the oil, add the onion and cabbage and sauté for 5 mins until softened.",
+                        "Add the mushrooms and some spring onions, and sauté for 1 more min.",
+                        "Pour in the remaining sesame oil and the noodles.",
+                        "If using cold noodles, let them heat through before adding the ingredients for the sauce – otherwise tip in straight away and keep stir-frying until sticky and piping hot.",
+                        "Sprinkle with the remaining spring onions."
+                    ),
+                    "https://www.themealdb.com/images/media/meals/wrustq1511475474.jpg"
+                )
+            )
+            add(
+                Meal(
+                    "Matar Paneer",
+                    "Vegetarian",
+                    "Indian",
+                    listOf(
+                        "Heat the oil in a frying pan over high heat until it’s shimmering hot.",
+                        "Add the paneer, then turn the heat down a little.",
+                        "Fry until it starts to brown at the edges, then turn it over and brown on each side – the paneer will brown faster than you think, so don’t walk away.",
+                        "Remove the paneer from the pan and drain on kitchen paper.",
+                        "Put the ginger, cumin, turmeric, ground coriander and chilli in the pan, and fry everything for 1 min.",
+                        "Add the tomatoes, mashing them with the back of a spoon and simmer everything for 5 mins until the sauce smells fragrant.",
+                        "Add a splash of water if it’s too thick.",
+                        "Season well.",
+                        "Add the peas and simmer for a further 2 mins, then stir in the paneer and sprinkle over the garam masala.",
+                        "Divide between two bowls, top with coriander leaves and serve with naan bread, roti or rice."
+                    ),
+                    "https://www.themealdb.com/images/media/meals/xxpqsy1511452222.jpg"
+                )
+            )
+            add(
+                Meal(
+                    "Chickpea Fajitas",
+                    "Vegetarian",
+                    "Mexican",
+                    listOf(
+                        "Heat oven to 200\u00B0C/180\u00B0C fan/gas 6 and line a baking tray with foil.",
+                        "Drain the chickpeas, pat dry and tip onto the prepared baking tray.",
+                        "Add the oil and paprika, toss to coat, then roast for 20-25 mins until browned and crisp, shaking halfway through cooking.",
+                        "Meanwhile, put the tomatoes and onion in a small bowl with the vinegar and set aside to pickle.",
+                        "Put the avocado in another bowl and mash with a fork, leaving some larger chunks.",
+                        "Stir in the lime juice and season well.",
+                        "Mix the soured cream with the harissa and set aside until ready to serve.",
+                        "Heat a griddle pan until nearly smoking.",
+                        "Add the tortillas , one at a time, charring each side until hot with griddle lines.",
+                        "Put everything on the table and build the fajitas : spread a little of the harissa cream over the tortilla, top with roasted chickpeas, guacamole, pickled salsa and coriander, if you like.",
+                        "Serve with the lime wedges for squeezing over."
+                    ),
+                    "https://www.themealdb.com/images/media/meals/tvtxpq1511464705.jpg"
+                )
+            )
+            add(
+                Meal(
+                    "Pancakes",
+                    "Dessert",
+                    "American",
+                    listOf(
+                        "Put the flour, eggs, milk, 1 tbsp oil and a pinch of salt into a bowl or large jug, then whisk to a smooth batter.",
+                        "Set aside for 30 mins to rest if you have time, or start cooking straight away.",
+                        "Set a medium frying pan or crêpe pan over a medium heat and carefully wipe it with some oiled kitchen paper.",
+                        "When hot, cook your pancakes for 1 min on each side until golden, keeping them warm in a low oven as you go.",
+                        "Serve with lemon wedges and sugar, or your favourite filling.",
+                        "Once cold, you can layer the pancakes between baking parchment, then wrap in cling film and freeze for up to 2 months."
+                    ),
+                    "https://www.themealdb.com/images/media/meals/rwuyqx1511383174.jpg"
+                )
+            )
+            add(
+                Meal(
+                    "Tarte Tatin",
+                    "Dessert",
+                    "French",
+                    listOf(
+                        "Roll the pastry to a 3mm-thick round on a lightly floured surface and cut a 24cm circle, using a plate as a guide.",
+                        "Lightly prick all over with a fork, wrap in cling film on a baking sheet and freeze while preparing the apples.",
+                        "Heat oven to 180\u00B0C/160\u00B0C fan/gas 4.",
+                        "Peel, quarter and core the apples.",
+                        "Put the sugar in a flameproof 20cm ceramic Tatin dish or a 20cm ovenproof heavy-based frying pan and place over a medium-high heat.",
+                        "Cook the sugar for 5-7 mins to a dark amber caramel syrup that’s starting to smoke, then turn off the heat and stir in the 60g diced chilled butter.",
+                        "To assemble the Tarte Tatin, arrange the apple quarters very tightly in a circle around the edge of the dish first, rounded-side down, then fill in the middle in a similar fashion.",
+                        "Gently press with your hands to ensure there are no gaps.",
+                        "Brush the fruit with the melted butter.",
+                        "Bake in the oven for 30 mins, then remove and place the disc of frozen puff pastry on top – it will quickly defrost.",
+                        "Tuck the edges down the inside of the dish and, with a knife, prick a few holes in the pastry to allow steam to escape.",
+                        "Bake for a further 40-45 mins until the pastry is golden brown and crisp.",
+                        "Allow to cool to room temperature for 1 hr before running a knife around the edge of the dish and inverting it onto a large serving plate that is deep enough to contain the juices.",
+                        "Serve with crème fraîche or vanilla ice cream."
+                    ),
+                    "https://www.themealdb.com/images/media/meals/ryspuw1511786688.jpg"
+                )
+            )
+            add(
+                Meal(
+                    "Chocolate Souffle",
+                    "Dessert",
+                    "French",
+                    listOf(
+                        "Heat oven to 220\u00B0C/fan 200\u00B0C/gas 7 and place a baking tray on the top shelf.",
+                        "For the sauce, heat the cream and sugar until boiling.",
+                        "Remove from the heat, stir in the chocolate and butter until melted, then keep warm.",
+                        "Brush 6 x 150ml ramekins with melted butter, sprinkle with the 2 tbsp caster sugar, then tip out any excess.",
+                        "Melt the chocolate and cream in a bowl over a pan of simmering water, cool, then mix in the egg yolks.",
+                        "Whisk the egg whites until they hold their shape, then add the sugar, 1 tbsp at a time, whisking back to the same consistency.",
+                        "Mix a spoonful into the chocolate, then gently fold in the rest.",
+                        "Working quickly, fill the ramekins, wipe the rims clean and run your thumb around the edges.",
+                        "Turn oven down to 200\u00B0C/fan 180\u00B0C/gas 6, place the ramekins onto the baking tray, then bake for 8-10 mins until risen with a slight wobble.",
+                        "Don’t open the oven door too early as this may make them collapse.",
+                        "Once the soufflés are ready, dust with icing sugar, scoop a small hole from their tops, then pour in some of the hot chocolate sauce.",
+                        "Replace the lids and serve straight away."
+                    ),
+                    "https://www.themealdb.com/images/media/meals/twspvx1511784937.jpg"
                 )
             )
         }

b4987c15c8177dd2a70a7c687b4102a4b05c2aa3
Julio Rahman <julio.rahman@gmail.com>
2025-09-09 10:26:10 +0700

    chore: migrate build configuration to Kotlin DSL and update dependency management

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+}
+
+android {
+    namespace =  "com.kencur.lezat"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.kencur.lezat"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 7
+        versionName = "0.1.2"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.startup)
+    implementation(libs.material)
+    implementation(libs.glide)
+    implementation(libs.shimmer)
+    implementation(libs.rive)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
